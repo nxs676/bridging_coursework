89401827b25af39ac522f36795cb6d97f80779e8
Nikol Levunlieva <nxs676@student.bham.ac.uk>
2020-08-27 15:45:54 +0100

    Installed Bootstrap Theme Clean blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
@@ -7,4 +9,6 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('homepage.html', views.post_list, name='post_list'),
+    path('about.html', TemplateView.as_view(template_name="blog/about.html"))
 ]
