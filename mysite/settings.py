acb92bfeb941ee6fbb02aee67b293c9018d82a77
Nikol Levunlieva <nxs676@student.bham.ac.uk>
2020-08-27 16:14:43 +0100

    Revert "Hidden Django Secret Key"
    This reverts commit bedbaca8

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from mysite.secret import KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -23,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY
+SECRET_KEY = '442u3+&f@#-1ez7+%gw+amiv=*x_&#zt%3t4&(*@pf(26okjw#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
